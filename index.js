a22512509677ee3971e91cfaae50576cde3ae273
Griffin Dooley <dooleygriffin@gmail.com>
2022-05-18 13:06:36 -0400

    commit tree 80c27ef0b4b4ab9cda997ad0529d7a340c3ffe11

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function(parameter) {
+    return drivers.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function(parameter) {
+    return drivers.slice(2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(integer) {
+    return function(fare) {
+        return fare * integer
+    }
+}
+
+function fareDoubler(fare) {
+    return fare * 2
+}
+
+function fareTripler(fare) {
+    return fare * 3
+}
+
+function fareQuintupler(fare) {
+    return fare * 5
+}
+
+function selectDifferentDrivers(array, func) {
+    return func(array)
+}
